00ab160673d1d8e16486e37b4a7d9624fe6199c1
katherine <116115224+kaerulean@users.noreply.github.com>
2024-11-24 17:00:42 -0500

    tailwind does not work pls fix
    yeah
    ignore the mass of commented out code

---
+++ 2024-music-hackathon-frontend/src/components/pitch.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <audio
+            controls
+            loop
+            crossorigin="anonymous"
+            id="audio"
+            ref="audio"
+            src="/test.mp3"
+        ></audio>
+
+        <button @click="record" ref="start">create context (or record whatever)</button>
+        <button disabled>stop</button>
+        <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useTemplateRef } from 'vue';
+import Meyda from 'meyda';
+
+//const startButton = useTemplateRef('start')
+
+function record(){
+    const audioContext = new AudioContext();
+    const htmlAudioElement = document.getElementById('audio')
+    const source = audioContext.createMediaElementSource(htmlAudioElement);
+
+    source.connect(audioContext.destination);
+    //startButton.setAttribute("disabled", "disabled")
+
+    
+}
+//const audioContext = new AudioContext();
+// const htmlAudioElement = useTemplateRef('listen');
+// const source = audioContext.createMediaElementSource(htmlAudioElement);
+
+// source.connect(audioContext.destination);
+// console.log(useTemplateRef('listen'))
+
+
+// // on play the audio that has been loaded
+// audioPlayer.onplay = () => {
+//   // get a new AudioContext
+//   const audioCtx = new AudioContext();
+//   // get a source of the audio
+//   const source = audioCtx.createMediaElementSource(audioPlayer);
+//   // output the audio to the speakers
+//   source.connect(audioCtx.destination);
+//   }
+
+
+</script>
+
+<style scoped>
+
+</style>
--- 2024-music-hackathon-frontend/src/views/HomeView.vue
+++ 2024-music-hackathon-frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import SpeechRecog from '../components/SpeechRecog.vue'
+import pitch from '@/components/pitch.vue';
 </script>
 
 <template>
   <main>
     <SpeechRecog />
+    <pitch />
   </main>
 </template>
